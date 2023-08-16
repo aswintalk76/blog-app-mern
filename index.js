6ceeec4a8e0eb281e51d953eba4f335f08169466
Ashwani Raj Agrahari <77192787+aswintalk76@users.noreply.github.com>
2023-08-16 22:57:18 +0530

    add middelwares

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ connectDB();
 //rest objecct
 const app = express();
 
+//middelwares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
 //create a default route
 app.get("/", (req, res) => {
     res.send(`Server is working on ${port}`);
