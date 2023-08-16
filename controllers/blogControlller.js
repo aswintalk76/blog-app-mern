6d5ebe3d28c1d423a8913c1c0d6c7aab95e16d96
Ashwani Raj Agrahari <77192787+aswintalk76@users.noreply.github.com>
2023-08-16 23:37:09 +0530

    working on blogRoute

---
+++ controllers/blogControlller.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const blogModel = require("../models/blogModel");
+const userModel = require("../models/userModel");
+
+//GET ALL BLOGS
+exports.getAllBlogsController = async (req, res) => {
+     // console.log("getAllBlogsController")
+};
+
+//Create Blog
+exports.createBlogController = async (req, res) => {
+    // console.log("createBlogController")
+};
+
+//Update Blog
+exports.updateBlogController = async (req, res) => {
+    // console.log("updateBlogController")
+};
+
+//SIngle Blog
+exports.getBlogByIdController = async (req, res) => {
+    // console.log("getBlogByIdController")
+};
+
+//Delete Blog
+exports.deleteBlogController = async (req, res) => {
+     // console.log("deleteBlogController")
+};
+
+//GET USER BLOG
+exports.userBlogControlller = async (req, res) => {
+    // console.log("userBlogControlller")
+};
