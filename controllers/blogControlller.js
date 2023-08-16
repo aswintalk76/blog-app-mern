0de819c843079c3befc7c9b5bba8440d07937ae6
Ashwani Raj Agrahari <77192787+aswintalk76@users.noreply.github.com>
2023-08-17 00:27:51 +0530

    deleteBlogController

--- controllers/blogControlller.js
+++ controllers/blogControlller.js
@@ -107,6 +107,25 @@ exports.getBlogByIdController = async (req, res) => {
 //Delete Blog
 exports.deleteBlogController = async (req, res) => {
     // console.log("deleteBlogController")
+    try {
+        const blog = await blogModel
+            // .findOneAndDelete(req.params.id)
+            .findByIdAndDelete(req.params.id)
+            .populate("user");
+        await blog.user.blogs.pull(blog);
+        await blog.user.save();
+        return res.status(200).send({
+            success: true,
+            message: "Blog Deleted!",
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send({
+            success: false,
+            message: "Erorr WHile Deleteing BLog",
+            error,
+        });
+    }
 };
 
 //GET USER BLOG
