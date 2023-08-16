8b4430afb78d259f6f758ebe837984a16e9acca7
Ashwani Raj Agrahari <77192787+aswintalk76@users.noreply.github.com>
2023-08-17 00:28:45 +0530

    userBlogControlller

--- controllers/blogControlller.js
+++ controllers/blogControlller.js
@@ -131,4 +131,26 @@ exports.deleteBlogController = async (req, res) => {
 //GET USER BLOG
 exports.userBlogControlller = async (req, res) => {
     // console.log("userBlogControlller")
+    try {
+        const userBlog = await userModel.findById(req.params.id).populate("blogs");
+
+        if (!userBlog) {
+            return res.status(404).send({
+                success: false,
+                message: "blogs not found with this id",
+            });
+        }
+        return res.status(200).send({
+            success: true,
+            message: "user blogs",
+            userBlog,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send({
+            success: false,
+            message: "error in user blog",
+            error,
+        });
+    }
 };
