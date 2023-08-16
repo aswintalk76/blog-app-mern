f0215df1dc83345a00df8cb758653a138643c1a2
Ashwani Raj Agrahari <77192787+aswintalk76@users.noreply.github.com>
2023-08-16 22:38:19 +0530

    connect mongodb

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require("express");
-const app = express();
+const colors = require("colors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
 const port = process.env.port || 5000;
+
+//env config
+dotenv.config();
+
+//mongodb connection
+connectDB();
+
+//rest objecct
+const app = express();
 
 //create a default route
 app.get("/", (req, res) => {
     res.send(`Server is working on ${port}`);
 });
 
+//listen
 app.listen(port,()=>{
-    console.log(`Server is listening on port ${port}`);
+    console.log(`Server Running on ${process.env.DEV_MODE} mode port no ${port}`.bgCyan
+    .white);
 })
