ad33b5308a4a8834d218e9222e212d1865b85704
Ashwani Raj Agrahari <77192787+aswintalk76@users.noreply.github.com>
2023-08-17 00:26:29 +0530

    getBlogByIdController

--- controllers/blogControlller.js
+++ controllers/blogControlller.js
@@ -80,6 +80,28 @@ exports.updateBlogController = async (req, res) => {
 //SIngle Blog
 exports.getBlogByIdController = async (req, res) => {
     // console.log("getBlogByIdController")
+    try {
+        const { id } = req.params;
+        const blog = await blogModel.findById(id);
+        if (!blog) {
+            return res.status(404).send({
+                success: false,
+                message: "blog not found with this is",
+            });
+        }
+        return res.status(200).send({
+            success: true,
+            message: "fetch single blog",
+            blog,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send({
+            success: false,
+            message: "error while getting single blog",
+            error,
+        });
+    }
 };
 
 //Delete Blog
