9455355623e3fb146d9f3b73c3e913d08d05a608
Ashwani Raj Agrahari <77192787+aswintalk76@users.noreply.github.com>
2023-08-17 00:23:59 +0530

    createBlogController

--- controllers/blogControlller.js
+++ controllers/blogControlller.js
@@ -4,12 +4,51 @@ const userModel = require("../models/userModel");
 
 //GET ALL BLOGS
 exports.getAllBlogsController = async (req, res) => {
-     // console.log("getAllBlogsController")
+    // console.log("getAllBlogsController")
 };
 
 //Create Blog
 exports.createBlogController = async (req, res) => {
     // console.log("createBlogController")
+    try {
+        const { title, description, image, user } = req.body;
+        //validation
+        if (!title || !description || !image || !user) {
+            return res.status(400).send({
+                success: false,
+                message: "Please Provide ALl Fields",
+            });
+        }
+        const exisitingUser = await userModel.findById(user);
+        //validaton
+        if (!exisitingUser) {
+            return res.status(404).send({
+                success: false,
+                message: "unable to find user",
+            });
+        }
+
+        const newBlog = new blogModel({ title, description, image, user });
+        const session = await mongoose.startSession();
+        session.startTransaction();
+        await newBlog.save({ session });
+        exisitingUser.blogs.push(newBlog);
+        await exisitingUser.save({ session });
+        await session.commitTransaction();
+        await newBlog.save();
+        return res.status(201).send({
+            success: true,
+            message: "Blog Created!",
+            newBlog,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send({
+            success: false,
+            message: "Error WHile Creting blog",
+            error,
+        });
+    }
 };
 
 //Update Blog
@@ -24,7 +63,7 @@ exports.getBlogByIdController = async (req, res) => {
 
 //Delete Blog
 exports.deleteBlogController = async (req, res) => {
-     // console.log("deleteBlogController")
+    // console.log("deleteBlogController")
 };
 
 //GET USER BLOG
