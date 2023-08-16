dc0a484b7cab1cce7f41bb5b910a2286f7ba37e3
Ashwani Raj Agrahari <77192787+aswintalk76@users.noreply.github.com>
2023-08-16 23:20:40 +0530

    add registerController userController.js

--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -2,7 +2,42 @@ const userModel = require("../models/userModel");
 const bcrypt = require("bcrypt");
 //create user register user
 exports.registerController = async (req, res) => {
-    console.log("registerController")
+    // console.log("registerController")
+    try {
+        const { username, email, password } = req.body;
+        //validation
+        if (!username || !email || !password) {
+            return res.status(400).send({
+                success: false,
+                message: "Please Fill all fields",
+            });
+        }
+        //exisiting user
+        const exisitingUser = await userModel.findOne({ email });
+        if (exisitingUser) {
+            return res.status(401).send({
+                success: false,
+                message: "user already exisits",
+            });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        //save new user
+        const user = new userModel({ username, email, password: hashedPassword });
+        await user.save();
+        return res.status(201).send({
+            success: true,
+            message: "New User Created",
+            user,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            message: "Error In Register callback",
+            success: false,
+            error,
+        });
+    }
 };
 
 // get all users
